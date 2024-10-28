03a4a5351d9469f259d19f6567ace9d3e319c761
amit strix <amit.strix@gmail.com>
2024-10-28 15:14:41 +0530

    data-json

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+$url='user.json';
+$data=file_get_contents($url);
+$character=json_decode($data,true)
+?>
+<table class="table-data">
+    <thead>
+        <tr>
+            <th>Name</th>
+            <th>Skill</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php foreach($character as $key =>$value):?>
+        <tr></tr>
+        <td>
+            <?php echo $value['name']?>
+            <?php echo $value['skill']?>
+        </td>
+        
+        </tr>
+         
+         <?php endforeach ?>   
+    </tbody>
+</table>
+</body>
+</html>
